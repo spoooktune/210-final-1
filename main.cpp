c2ec1c563946487bcfe2d4e70a1dd45032ded123
spoooktune <asriprasana5@gmail.com>
2025-12-10 15:56:39 -0800

    Started code for reading from file and adding to map

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
 int main(){
-    
+    map<string, int> airport_traffic;
+    fstream flight_data;
+    flight_data.open("210-final-1-FA25.txt");
+    if (!flight_data.good()){
+        cout << "Unable to open file" << endl;
+    }
+    else{
+    while(!flight_data.eof())
+        string airport;
+        flight_data >> airport;
+        if (airport_traffic.empty()){
+            // add new airport
+        }
+    }
     return 0;
 }
