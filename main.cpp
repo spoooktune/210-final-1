6322a27e01eef8ba11183a46bac2b6a094435f4e
spoooktune <asriprasana5@gmail.com>
2025-12-10 16:16:45 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@ using namespace std;
 int main(){
     map<string, int> airport_traffic;
     fstream flight_data;
+    // Milestone 1
     flight_data.open("210-final-1-FA25.txt");
     if (!flight_data.good()){
         cout << "Unable to open file" << endl;
     }
     else{
     string airport;
-        while(flight_data >> airport){
-            bool counter_up = false;
+        while(!flight_data.eof()){
             flight_data >> airport;
+            bool counter_up = false;
             if (!airport_traffic.empty()){
                 for (auto &pair : airport_traffic){
                     if (pair.first == airport){
@@ -35,6 +36,7 @@ int main(){
         }
     }
 
+    // Milestone 1
     for (auto &pair : airport_traffic){
         cout << pair.first << " " << pair.second << endl;
     }
